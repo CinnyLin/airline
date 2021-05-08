a9df6f784b4f7a76d09e02b3adbf0a8ffc2e2377
CinnyLin <45028889+CinnyLin@users.noreply.github.com>
2021-05-08 15:57:51 +0800

    #num_tickets_left IS UPDATED WHEN PURCHASE!!

--- create_stored_procedures.sql
+++ create_stored_procedures.sql
@@ -1,12 +1,6 @@
 -- define trigger which updates num_tickets_left
 
-DROP trigger IF EXISTS add_tickets;
-CREATE trigger add_tickets BEFORE INSERT ON ticket
-for each ROW
-	UPDATE flight
-    SET num_tickets_left = num_tickets_left + 1
-    WHERE flight.airline_name = NEW.airline_name and flight.flight_num = NEW.flight_num;
-
+-- deletes tickets when customer or agent make purchase
 DROP trigger IF EXISTS delete_tickets;
 CREATE trigger delete_tickets AFTER INSERT ON purchase
 for each ROW 
